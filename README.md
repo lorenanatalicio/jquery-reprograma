deb5a6d96ecd0e411f6d54bd36447137965c6a5c
lorena natalicio <lorena.natalicio@gmail.com>
2019-04-19 18:54:57 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Curso online de Front-end {Reprograma}
+
+### Exercício da aula de JQuery
+
+#### Carousel criado no curso de Front-end {Reprograma}.
---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+$(function () {
+    $('#carousel img:eq(0)').addClass('ativo').show();
+    let legenda = $('.ativo').attr('alt');
+    $('#carousel').prepend("<p>" + legenda + "</p>");
+    setInterval(slider, 3500);
+    // $('#carousel').click(function(){
+
+
+    function slider() {
+        if ($('.ativo').next().length) {
+
+            $('.ativo').fadeOut().removeClass('ativo').next().fadeIn().addClass('ativo');
+
+        } else {
+
+            $('.ativo').fadeOut().removeClass('ativo');
+            $('#carousel img:eq(0)').fadeIn().addClass('ativo');
+
+        }
+        
+        let legenda = $('.ativo').attr('alt');
+        $('#carousel p').hide().html(legenda).delay(150).fadeIn();
+    }
+});
